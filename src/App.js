1e84c9f581465f82a6f3956d2d30b5b69a9049c7
Akash patel <akashpatel10000@gmail.com>
2022-01-15 15:46:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  // states for age and name and friends
+  const [name, setName] = useState("");
+  const [age, setAge] = useState();
+  const [friends, setFriends] = useState([]);
+
+  // function which send data to the server
+  const handleSubmit = () => {
+    Axios.post("http://localhost:3001/addfriend", { name: name, age: age })
+      .then((res) => {
+        alert("Sucessfully send to the server");
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  // Function to delete friend
+  const handleDelete = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`)
+      .then((res) => {
+        alert("Delete request sended to the server");
+        setFriends(
+          friends.filter((ele) => {
+            return ele._id !== id;
+          })
+        );
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  // function which update the data of friend
+  const handleUpdate = (id) => {
+    const newAge = prompt("Enter new age:");
+    Axios.put("http://localhost:3001/update", { id: id, newAge: newAge })
+      .then((res) => {
+        alert("updated data sended to the server");
+        setFriends(
+          friends.map((ele) => {
+            return ele._id === id
+              ? { _id: id, name: ele.name, age: newAge }
+              : ele;
+          })
+        );
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+  // Extrating all friend from database
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read")
+      .then((response) => {
+        setFriends(response.data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Friends>
+        {friends.map((ele) => {
+          return (
+            <div>
+              <h2>Name: {ele.name}</h2>
+              <p>Age: {ele.age}</p>
+              <Button
+                onClick={() => {
+                  handleUpdate(ele._id);
+                }}
+              >
+                Update
+              </Button>
+              <Button
+                onClick={() => {
+                  handleDelete(ele._id);
+                }}
+              >
+                Delete
+              </Button>
+            </div>
+          );
+        })}
+      </Friends>
+      <Form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+          type="text"
+          placeholder="Friend Name"
+        />
+        <input
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+          value={age}
+          type="number"
+          placeholder="Friend Age"
+        />
+        <Button type="submit">Add Friend</Button>
+      </Form>
+    </Container>
   );
 }
 
 export default App;
+const Container = styled.div`
+  height: 100vh;
+  padding: 10px;
+`;
+const Friends = styled.div`
+  div {
+    background-color: lightgreen;
+    margin-bottom: 5px;
+  }
+  h2 {
+    font-size: 18px;
+    padding: 5px 10px;
+  }
+  p {
+    font-size: 16px;
+    padding: 5px 10px;
+  }
+  Button {
+    padding: 5px;
+  }
+`;
+const Form = styled.form`
+  margin-top: 20px;
+  input {
+    display: block;
+    margin: 10px;
+    padding: 10px;
+    font-size: 14px;
+    outline: none;
+  }
+`;
+const Button = styled.button`
+  padding: 10px;
+  margin: 10px;
+  font-size: 14px;
+  cursor: pointer;
+  background-color: green;
+  border: none;
+  color: white;
+`;
